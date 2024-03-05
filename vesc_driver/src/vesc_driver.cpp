4e4e39691718803574caaac5c690436afe38c5db
Philip Wette <philip.wette@hsbi.de>
2024-03-05 19:58:57 +0100

    correct units for imu message

--- vesc_driver/src/vesc_driver.cpp
+++ vesc_driver/src/vesc_driver.cpp
@@ -235,13 +235,15 @@ void VescDriver::vescPacketCallback(const std::shared_ptr<VescPacket const> & pa
     imu_msg.imu.orientation.y = imuData->q_y();
     imu_msg.imu.orientation.z = imuData->q_z();
 
-    std_imu_msg.linear_acceleration.x = imuData->acc_x();
-    std_imu_msg.linear_acceleration.y = imuData->acc_y();
-    std_imu_msg.linear_acceleration.z = imuData->acc_z();
+    //converted from g to m/s^2
+    std_imu_msg.linear_acceleration.x = imuData->acc_x() / 9.80665;
+    std_imu_msg.linear_acceleration.y = imuData->acc_y() / 9.80665;
+    std_imu_msg.linear_acceleration.z = imuData->acc_z() / 9.80665;
 
-    std_imu_msg.angular_velocity.x = imuData->gyr_x();
-    std_imu_msg.angular_velocity.y = imuData->gyr_y();
-    std_imu_msg.angular_velocity.z = imuData->gyr_z();
+    //converted from deg/s to rad/s
+    std_imu_msg.angular_velocity.x = imuData->gyr_x() / 180.0 * M_PI;
+    std_imu_msg.angular_velocity.y = imuData->gyr_y() / 180.0 * M_PI;
+    std_imu_msg.angular_velocity.z = imuData->gyr_z() / 180.0 * M_PI;
 
     std_imu_msg.orientation.w = imuData->q_w();
     std_imu_msg.orientation.x = imuData->q_x();
